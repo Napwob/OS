f2ecd8a2ebe2a40243114c15e51143b87fe02732
Kadconner <47683913+Kadconner@users.noreply.github.com>
2020-09-23 18:33:19 +0700

    fixili programmu

--- lab1/project.sh
+++ lab1/project.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "No arguments"
+  exit
+fi
+
 if [ -n "$1" ]
 then
 
@@ -25,19 +31,36 @@ echo "    bash lab1.sh -h"
 
 -h) echo "Авторы: Крюков Илья Петрович ИВ-822, Русманов Владислав Владимирович ИВ-822, Сысоева Анастасия ИВ-822" 
 echo "Краткое описание проекта:"
+echo "Данный скрипт при запуске с ключём "--help", либо "-h" выводит справку,"
+echo "Если скрипт был запущен без ключей, то выводится сообщение об ошибке."
+echo "При запуске данного скрипта с ключом -alnetin производится вывод всех сетевых интерфейсов,"
+echo "с ключом -intuoff(...) производится отключение заданных сетевых интерфейсов,"
+echo "с ключом -intuon(...) включение заданных сетевых интерфейсов"
+echo "Все доступные аргументы:"
+echo "Все доступные аргументы:"
+echo "    --help"
+echo "    -h"
+echo "    -alnetin"
+echo "    -intuoff"
+echo "    -intuon"
+echo "Примеры запуска:"
+echo "    bash lab1.sh --help"
+echo "    bash lab1.sh -h"
 
 ;;
 
+-kproc)
+    kill -9 $(lsof -t -i4:$2);;
 
 
 
--alnetin) ifconfig;; #вывод всех сетевых интерфейсов
+-alnetin) sudo ifconfig;; #вывод всех сетевых интерфейсов
 
 -intuoff) if [ -n "$2" ] # отключение заданных интерфейсов
 	then
 		while [ -n "$2" ]
 		do
-		ifconfig $2 down
+		sudo ifconfig $2 down
 		shift
 		done
 	else
@@ -49,7 +72,7 @@ echo "Краткое описание проекта:"
 then
 while [ -n "$2" ]
 do
-ifconfig $2 up
+sudo ifconfig $2 up
 shift
 done
 else
@@ -60,7 +83,7 @@ fi;;
 	then 
 		if [-n "$3"]
 then 
-			ifconfig $2 $3
+			sudo ifconfig $2 $3
 		else
 		echo "Too few arguments"
 		fi
@@ -72,7 +95,7 @@ then
 then 
 		if [-n "$3"]
 then 
-			ifconfig $2 netmask $3
+			sudo ifconfig $2 netmask $3
 		else
 		echo "Too few arguments"
 		fi
@@ -85,7 +108,7 @@ then
 then 		
 if [-n "$3"]
 then 
-			ifconfig $2 gateway $3
+			sudo ifconfig $2 gateway $3
 		else
 		echo "Too few arguments"
 		fi
@@ -94,7 +117,8 @@ then
 	fi;;
 
 
-
+-netstatic)
+    cat /proc/net/dev;;
 
 
 *) echo "Error argument" ;;
